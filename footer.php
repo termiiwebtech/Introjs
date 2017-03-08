871bd27b9343cb3b55c28826dfdef2a3aa46d2fc
termii <gbolademmanuel@gmail.com>
2017-03-08 13:47:17 +0100

    Introjs Commit
    New commit for introjs

---
+++ footer.php
@@ -0,0 +1,25 @@
+<div class="pusher"></div>
+
+
+
+	<!-- Bottom scripts (common) -->
+ <script src="<?php echo base_url('assets/js/gsap/main-gsap.js'); ?>"></script>
+ <script src="<?php echo base_url('assets/js/joinable.js'); ?>"></script>
+ <script src="<?php echo base_url('assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js'); ?>"></script>
+ <script src="<?php echo base_url('assets/js/resizeable.js'); ?>"></script>
+ <script src="<?php echo base_url('assets/js/neon-api.js'); ?>"></script>
+ <script src="<?php echo base_url('assets/js/jquery.validate.min.js'); ?>"></script>
+ <script src="<?php echo base_url('assets/js/neon-login.js'); ?>"></script>
+ <script src="<?php echo base_url('assets/js/neon-register.js'); ?>"></script>
+ <script src="<?php echo base_url('assets/js/jquery.inputmask.bundle.min.js'); ?>"></script>
+ <script src="<?php echo base_url('assets/js/intro.js'); ?>"></script>
+
+
+	<!-- JavaScripts initializations and stuff -->
+ <script src="<?php echo base_url('assets/js/neon-custom.js'); ?>"></script>
+
+
+	<!-- Demo Settings -->
+	 <script src="<?php echo base_url('assets/js/neon-demo.js'); ?>"></script>
+
+
